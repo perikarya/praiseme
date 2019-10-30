4c70d928a59bddd2e10870e7a1290daeff33cd6a
perikarya <40226185+perikarya@users.noreply.github.com>
2019-10-31 07:34:38 +0800

    Add files via upload
    added tests

---
+++ tests/testthat/test-praiseme.R
@@ -0,0 +1,3 @@
+test_that("correct praise is given", {
+  expect_equivalent(praiseme("art"), "You're great at art!")
+})
---
+++ tests/testthat/test-praiseme_graphics.r
@@ -0,0 +1,3 @@
+test_that("the praise makes sense", {
+  expect_error(praiseme_graphics(54321))
+})
